6d11b98f9b80d90f9a73dda9b594772954668e41
Olakunle Boye <boye@sprinble.co>
2020-10-09 06:44:14 +0100

    refactor project structure

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+// const webpack = require('webpack');
+const mix = require('laravel-mix')
+
+mix.disableSuccessNotifications()
+mix.js('src/app.js', 'dist/js')
+    // .js('src/lib/vendor.js', 'dist/js')
+// .sass('resources/sass/app.scss', 'public/css');
+    .styles([
+        'node_modules/admin-lte/plugins/fontawesome-free/css/all.css',
+        'node_modules/admin-lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.css',
+        'node_modules/admin-lte/dist/css/adminlte.css',
+        'node_modules/nprogress/nprogress.css',
+        'node_modules/admin-lte/plugins/select2/css/select2.css',
+        'node_modules/admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.css',
+        'node_modules/admin-lte/plugins/toastr/toastr.css',
+        'node_modules/vue-date-pick/dist/vueDatePick.css'
+    ], 'dist/css/vendor.css')
+
+// mix.browserSync('t40-backoffice.test')
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.(js|vue)$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+})
