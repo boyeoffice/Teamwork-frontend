34cfc9a11bff3d9b957eae7be1db4accf6839d73
Olakunle Boye <boye@sprinble.co>
2020-10-10 10:44:08 +0100

    login function

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -8,9 +8,12 @@
           <form @submit.prevent="login">
             <div class="form-group input-group mb-3"><input type="email" placeholder="Email" class="form-control" v-model="form.email"> <div class="input-group-append"><div class="input-group-text"><span class="fas fa-envelope"></span></div></div> <!----></div>
             <div class="input-group mb-3"><input type="password" placeholder="Password" class="form-control" v-model="form.password"> <div class="input-group-append"><div class="input-group-text"><span class="fas fa-lock"></span></div></div> <!----></div>
-            <div class="social-auth-links text-center mb-3"><button type="submit" :disabled="!formValid" class="btn btn-block btn-danger">
+            <div class="social-auth-links text-center mb-3">
+                <button v-if="isButton" type="submit" :disabled="!formValid" class="btn btn-block btn-danger">
                 Login
-            </button></div>
+                </button>
+                <button v-else :disabled="disabled" class="btn btn-danger btn-block">Login</button>
+            </div>
             <div class="text-center"><span class="error invalid-feedback d-block"></span></div>
           </form>
         </div>
@@ -21,24 +24,29 @@
 
 <script>
 import { reactive, computed } from '@vue/composition-api'
+import router from '../../router'
 export default {
-    setup () {
+    setup (_, context) {
         const form = reactive({
             email: '',
             password: ''
         })
+        let isButton = true
         const formValid = computed(() => {
             return Object.keys(form).every(field => {
                 return form[field]
             })
         })
         function login () {
-            console.log(form.email)
+            // console.log(form.email)
+            isButton = false
+            router.push('/')
         }
         return {
             form,
             login,
-            formValid
+            formValid,
+            isButton
         }
     }
 }
